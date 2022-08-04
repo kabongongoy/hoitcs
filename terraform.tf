8c4dbe064fdeb196095d458dab5da3cd0f9f6294
Henry Faleye <henry.faleye@cmd.com.au>
2022-08-04 18:11:52 +1000

    issue 2

---
+++ terraform.tf
@@ -0,0 +1,36 @@
+terraform {
+  #backend "s3" {} # Uncomment this once the backend has been deployed.
+   backend "local" {}
+ 
+ 
+  required_providers {
+    aws = {
+      version = "4.24.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
+# provider "aws" {
+#   region = "us-east-1"
+#   profile = "hoitcs-api"
+#   #profile = "vscode"
+# }
+provider "aws" {
+  profile = "hoitcs-api"                                                 #local.workspace["aws_profile"]
+  region  = var.region
+  access_key = "AKIA4W3EF52DJCUXBGVM"
+  secret_key = "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"
+
+    default_tags {
+      tags = {
+        # Repository       = local.env["global"]["tags"]["Repository"]
+        Workspace        = local.env["global"]["tags"]["Workspace"]
+        Matter           = local.env["global"]["tags"]["Matter"]
+        Purpose          = local.env["global"]["tags"]["Purpose"]
+        Project          = local.env["global"]["tags"]["Project"]
+        Function         = local.env["global"]["tags"]["Function"]
+        # BusinessOwner    = local.env["global"]["tags"]["BusinessOwner"]
+        # ResponsibleParty = local.env["global"]["tags"]["ResponsibleParty"]
+    }
+  }
+}
